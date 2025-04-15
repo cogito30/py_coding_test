a474f309a4f2dca8a57a17526601d63c738abdca
cogito30 <cogito30g@gmail.com>
2025-04-15 15:40:12 +0900

    feat: (Lv1) 12931 12932 12937 12944

---
+++ Lv1/12931.py
@@ -0,0 +1,7 @@
+# 자릿수 더하기
+def solution(n):
+    answer = 0
+    while n > 0:
+        answer += n % 10
+        n = n // 10
+    return answer
---
+++ Lv1/12932.py
@@ -0,0 +1,7 @@
+# 자연수 뒤집어 배열로 만들기
+def solution(n):
+    answer = []
+    while n > 0:
+        answer.append(n % 10)
+        n //= 10
+    return answer
