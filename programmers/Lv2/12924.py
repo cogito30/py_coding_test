6376ee3804fb76311a8b8c86438be96f4494930a
cogito30 <cogito30g@gmail.com>
2025-05-10 01:16:26 +0900

    feat: (programmers Lv2) 18문제

---
+++ programmers/Lv2/12924.py
@@ -0,0 +1,15 @@
+# 숫자의 표현
+def solution(n):
+    answer = 0
+    count = 0
+    for i in range(1, n+1):
+        total = 0
+        for j in range(i, n+1):
+            total += j
+            if total == n:
+                count += 1
+                break
+            elif total > n:
+                break
+    answer = count
+    return answer
